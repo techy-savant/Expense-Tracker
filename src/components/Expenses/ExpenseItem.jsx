bdae040f27b55e090c46f29b244d5aabdeeea7cd
techy-savant <ogbanufavour@gmail.com>
2024-01-17 16:52:19 +0100

    added conditional return logic

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,22 +2,19 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = (props)=>{
- 
-  const clickHandler = ()=>{
-    setTitle('Updated'); 
-  }
-  return ( 
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+const ExpenseItem = (props) => {
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
 
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-} 
+};
 
 export default ExpenseItem;
